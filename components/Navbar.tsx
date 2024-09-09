96144fd7de2116b12b2a3035aea8897b95c3992e
gosm1 <109235374+gosm1@users.noreply.github.com>
2024-09-09 01:17:50 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav className='blurBackground z-20 top-0 sticky  py-4 flex items-center justify-around '>
             <a href='/'><Image src={logo} alt='logo' className='w-[150px] md:w-[230px]'/></a>
-            <ul className='flex justify-center items-center gap-8 text-black font-medium'>
+            <ul className='flex justify-center items-center gap-4 md:gap-8 text-black font-medium'>
                 <li><a href='/'>Home</a></li>
                 <li><a href='/perfume-recommendation'>Recommendation</a></li>
             </ul>
